3d230dc70c0541de85bd5654c0471c6f05eb76b2
Maximilian Remming <maximilian@remming.fi>
2023-05-19 23:23:39 +0300

    Move divTemplate to its own file

--- esox/forms/form_render.go
+++ esox/forms/form_render.go
@@ -1,44 +1,14 @@
 package forms
 
 import (
+	_ "embed"
 	"html/template"
 
 	"github.com/xremming/abborre/esox/utils"
 )
 
-var divTemplate = `
-{{ range .Form.Fields }}
-<div class="{{ $.FieldClass }}">
-
-  <label for="{{ .ID }}">{{ .Label }}{{ if .Required }}*{{ end }}</label>
-
-  {{ if eq .Kind "text" }}
-    {{ if .Config.Multiline }}
-    <textarea id="{{ .ID }}" name="{{ .Name }}">{{ .Value }}</textarea>
-    {{ else }}
-    <input id="{{ .ID }}" type="text" name="{{ .Name }}" value="{{ .Value }}" />
-    {{ end }}
-  {{ else if eq .Kind "datetime-local" }}
-  <input id="{{ .ID }}" type="datetime-local" name="{{ .Name }}" value="{{ .Value }}" />
-  {{ else if eq .Kind "select" }}
-  <select id="{{ .ID }}" name="{{ .Name }}">
-  {{ range .Options }}
-    <option value="{{ .Value }}"{{ if .Selected }} selected{{ end }}>{{ .Label }}</option>
-  {{ end }}
-  </select>
-  {{ end }}
-
-  {{ range .Errors }}
-  <div class="{{ $.ErrorClass }}">{{ . }}</div>
-  {{ end }}
-
-  </div>
-{{ end }}
-
-{{ range .Form.Errors }}
-<div class="{{ $.ErrorClass }}">{{ . }}</div>
-{{ end }}
-`
+//go:embed templates/form_div.html
+var divTemplate string
 
 var div = template.Must(template.New("div").Parse(divTemplate))
 

42046e9f8653c2802bc3f74866ae362930135885
Maximilian Remming <maximilian@remming.fi>
2023-05-21 03:45:12 +0300

    Add temporary environment variables to lambda functions

--- infra/homepage/lambda.tf
+++ infra/homepage/lambda.tf
@@ -3,6 +3,10 @@ resource "aws_cloudwatch_log_group" "homepage" {
   retention_in_days = 14
 }
 
+locals {
+  base_url_suffix = var.env == "prod" ? "" : "-${var.env}"
+}
+
 resource "aws_lambda_function" "homepage" {
   filename         = "../abborre-lambda.zip"
   source_code_hash = filebase64sha256("../abborre-lambda.zip")
@@ -18,6 +22,15 @@ resource "aws_lambda_function" "homepage" {
   environment {
     variables = {
       TABLE_NAME = aws_dynamodb_table.homepage.name
+      BASE_URL   = "https://abborre${local.base_url_suffix}.remming.org"
+
+      // TODO: get SECRETS, OAUTH_CLIENT_ID and OAUTH_CLIENT_SECRET from AWS Secrets Manager
+      SECRETS             = "unsafe-abba1234"
+      OAUTH_CLIENT_ID     = ""
+      OAUTH_CLIENT_SECRET = ""
+
+      // TODO: remove once discord login is in place
+      ADMIN_PASSWORD = "maximilian94"
     }
   }
 

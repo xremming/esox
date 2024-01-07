a63916ce9d0392e985c4b14f5c0b16b0d362d111
Maximilian Remming <maximilian@remming.fi>
2024-01-07 04:40:00 +0200

    Add MaxAge to few pages

--- views/home.go
+++ views/home.go
@@ -2,6 +2,7 @@ package views
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/xremming/abborre/esox"
 )
@@ -10,7 +11,7 @@ var homeTmpl = esox.GetTemplate("home.html", "base.html")
 
 func Home() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		homeTmpl.Render(w, r, 200, &data{Public: true})
+		homeTmpl.Render(w, r, 200, &data{Public: true, MaxAge: 1 * time.Hour})
 	}
 }
 
@@ -18,6 +19,6 @@ var storyTmpl = esox.GetTemplate("story.html", "base.html")
 
 func Story() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		storyTmpl.Render(w, r, 200, &data{Public: true})
+		storyTmpl.Render(w, r, 200, &data{Public: true, MaxAge: 1 * time.Hour})
 	}
 }
